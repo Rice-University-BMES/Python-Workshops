bf31d616e922a0a4315468a6b7bb4e759f4055c7
carriganh <cdh5@rice.edu>
2021-02-04 18:38:35 -0600

    Initial commit

---
+++ workshop1.py
@@ -0,0 +1,51 @@
+# Comments! Use comments to explain your code.
+
+# Variables
+# Can contain letters, numbers, underscores
+# Best practice: separate words with underscores, use meaningful names
+
+current_year = 2021
+last_year = 2020
+print(current_year)
+print(last_year)
+print(current_year, last_year)
+print('current year:', current_year)
+# Show how to refactor variables
+
+# Data types
+
+# integer
+print(1)
+# float
+print(1.1)
+
+print(int(1.1))
+print(int(1.7))
+print(float(1))
+
+# string
+s1 = 'Python is fun'
+s2 = "yay Python"  # note that you can use double or single quotes
+
+print(s1)
+print(s2)
+
+s3 = '''Use triple quotes
+for multiple
+lines!'''
+
+print(s3)
+
+# Arithmetic operations
+print(2 + 2)
+print(10 - 3)
+print(5 * 4)
+print(5 / 2)
+# integer division
+print(5 // 2)
+print(2 ** 3)
+
+# Best practice: use parentheses, don't rely on pemdas
+
+
+# Functions: remember order of operations and indentation
