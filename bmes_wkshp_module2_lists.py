41d4e83e0d71936628a0814e4ef78b9da74bcc87
Eric T <ericdtorres@hotmail.com>
2021-02-08 14:57:29 -0600

    Added module 2 files

---
+++ bmes_wkshp_module2_lists.py
@@ -0,0 +1,69 @@
+# module 2 - Lists
+
+#author Eric Torres
+#email edt3@rice.edu
+#date 03 Feb 2021
+
+# Create
+
+mt_list = []
+print(mt_list)
+
+# order matters in a list! order is preserved
+# duplicate entries may exist in a list
+numbers = [1, 3, 4, -7, 3, 43]
+print(numbers)
+
+groceries = ['milk', 'eggs', 'bread', 'butter']
+print(groceries)
+
+# ask how many elements are in this list
+nested = [['b', 'i', 'o', 'e'], [2, 5, 2], []]
+print(nested)
+
+print("===")
+
+# Access
+
+print(len(groceries))
+print(len(nested))
+
+print("===")
+
+print (groceries[0])
+print (groceries[-1])
+print(nested[2])
+
+essentials = groceries[2:4]
+print(essentials)
+ess2 = groceries[2:]
+print(ess2)
+
+print("===")
+
+# Update
+
+groceries[2] = 'cheese'
+print(groceries)
+
+nested[1] = [3, 2, 2]
+print(nested)
+
+# elements of lists can be changed, unlike strings
+# brackets on right side of equals sign means access, brackets on left side means update
+# note that good programming means homogenous lists! however, you can have different data types in a list
+
+# List Methods
+
+# like other python data types, there are built-in methods (aka functions) that can modify them!
+
+classes = ['bioe 452', 'bioe 421']
+print(classes)
+
+classes.append('elec 327')
+print(classes)
+
+# in breakout rooms, can figure out what .reverse(), .insert() do
+
+
+# 
