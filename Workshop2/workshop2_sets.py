e25aef6ed22475c83fd32f40abc3b8faf51646b6
Eric T <ericdtorres@hotmail.com>
2021-02-26 00:54:13 -0600

    changed comments

--- Workshop2/workshop2_sets.py
+++ Workshop2/workshop2_sets.py
@@ -1,4 +1,4 @@
-# Python Workshop 2 - lists
+# Python Workshop 2 - sets
 
 # author Eric Torres
 # email edt3@rice.edu
@@ -14,7 +14,8 @@
 
 ### Sets
 
-courses = set([252, 332, 370, 391]) # can also be written as {}
+# courses = {252, 332, 370, 391}
+courses = set([252, 332, 370, 391])
 print(courses)
 
 courses2 = set([252, 332, 252, 370, 391, 332])
@@ -22,9 +23,12 @@ print(courses2)
 
 print(courses == courses2)
 
+print("===")
+
 for course in courses:
     print(course)
 
+print("===")
 
 courses.add(253)
 print(courses)
@@ -38,18 +42,20 @@ print(courses)
 
 # check if something is in a set
 print(369 in courses)
-print(332 in courses)
+print(332 not in courses)
 print(252 in courses)
+
+print("===")
 
 # you can perform logical operations on sets to combine or remove items!
 print(courses)
 print(courses2)
 
-courses3 = courses.union(courses2) # adds all items together into a set
+courses3 = courses.union(courses2)  # adds all items together into a set
 print(courses3)
-print(courses) # note that courses was not mutated!
+print(courses)  # note that courses was not mutated!
 
-courses3 = courses.intersection(courses2) # shows only items they have in common
+courses3 = courses.intersection(courses2)  # shows only items they have in common
 print(courses3)
 
 # courses3 = courses # what happens if i uncomment this line?
