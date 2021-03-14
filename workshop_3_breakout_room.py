b613bb5e8781caedd3b610dd892671b0ae9d76f4
Eric T <ericdtorres@hotmail.com>
2021-03-14 16:10:26 -0500

    added code for plot example

--- workshop_3_breakout_room.py
+++ workshop_3_breakout_room.py
@@ -11,7 +11,15 @@
 # import the numpy package and matplotlib.pyplot
 
 # create an array from 0 to 50 with a step size of 6
-# using numpy.arange()
+# using numpy.arrange()
 
 # using list comprehension, store all of the odd numbers in a new list
 
+# using plt.scatter, plot the data from this file!
+data = np.genfromtxt('hw0_data.txt', delimiter='\t')
+
+x2 = [x[2] for x in data if x[2] != np.nan]  # note: this is using numpy imported as 'np'
+y2 = [x[3] for x in data if x[3] != np.nan]
+
+x2.pop(0)
+y2.pop(0)
